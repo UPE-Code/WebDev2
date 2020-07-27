347a46aa75086a4a0a7ef78a108dbf797c86738a
DaJuan Harris <dajuancancode@gmail.com>
2020-07-26 23:37:10 -0400

    Resolving merge conflicts

---
+++ Types_and_Operators/boolean.js
@@ -0,0 +1,25 @@
+//Boolean: Represents true or false
+console.log(true)
+console.log(false)
+
+// Comparison Operators
+console.log(3 > 5)
+console.log(5 < 7)
+console.log(6 >= 6)
+console.log(9 <= 8)
+console.log(2 != 1)
+console.log(3 !== 2) // Why are there two not equal operators
+
+// Equality Operators
+console.log(2 == 3)
+console.log(5 === 5)
+
+// The difference between a double equals and a tripple equals, is  that a double equals will perform type conversion
+
+console.log(1 == "1") // true
+console.log(1 === "1") // false
+
+// Logical Operators
+console.log(!false)
+console.log(true && true)
+console.log(false || true)
---
+++ Types_and_Operators/numbers.js
@@ -0,0 +1,18 @@
+// Numberss: Represent integers and floating point numbers
+console.log(42)
+console.log(9.81)
+
+// There is a limit to the percision of a Javascript number
+console.log(5.4383748913790997376512351314511111)
+
+// There are many operations that can be performed on a number in Javascript
+console.log(3 + 3) // Addition 
+console.log(4 - 6) // Subtraction
+console.log(9 * 2) // Multiplication
+console.log(7 / 3) // Division
+console.log(8 % 2) // Modluo
+console.log(3 ** 2) // Exponential
+
+// NaN is a unique number type value that represents something that is not a number
+console.log(3 * "j")
+console.log(typeof NaN)
